ecbba60c697cbaeb940bd891fd850727221d96c7
mxcrpntr <carpenter.max@gmail.com>
2023-07-13 17:48:17 -0400

    last commit

--- practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/app/controllers/api/items_controller.rb
+++ practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/app/controllers/api/items_controller.rb
@@ -1,2 +1,34 @@
 class Api::ItemsController < ApplicationController
+
+    def index
+        @items = Item.all
+        render json: @items
+    end
+
+    def show
+        @item = Item.find(params[:id])
+        render json: @item
+    end
+
+    def create
+        @item = Item.new(item_params)
+        if @item.save
+            render :show
+        else
+            flash[:errors] = @item.errors.full_messages
+        end
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    def item_params
+        params.require(:item).permit(:pokemon_id,:name,:price,:happiness,:image_url)
+    end
 end
